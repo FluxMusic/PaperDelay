84d16c23c7750e84775a036f49e2fb5bfbf06d36
FluxMusic <alex.huber743@gmail.com>
2024-05-05 17:53:38 +0200

    added basic Rotary Slider class skeleton

---
+++ Source/Components/PaperKnob.h
@@ -0,0 +1,18 @@
+/*
+  ==============================================================================
+
+    PaperKnob.h
+    Created: 5 May 2024 5:41:45pm
+    Author:  Alex Huber
+
+  ==============================================================================
+*/
+
+#pragma once
+
+#include <JuceHeader.h>
+
+class PaperKnob : juce::Component
+{
+    void paint (juce::Graphics&) override;
+};
