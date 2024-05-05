58a0a9a34219ac78d938ec5073e7252d9f7dc7a9
FluxMusic <alex.huber743@gmail.com>
2024-05-05 17:50:39 +0200

    added global LookAndFeel class skeleton

---
+++ Source/LookAndFeel/PaperLookAndFeel.cpp
@@ -0,0 +1,16 @@
+/*
+  ==============================================================================
+
+    PaperLookAndFeel.cpp
+    Created: 5 May 2024 5:42:04pm
+    Author:  Alex Huber
+
+  ==============================================================================
+*/
+
+#include "PaperLookAndFeel.h"
+
+void PaperLookAndFeel::drawRotarySlider(juce::Graphics& g, int x, int y, int width, int height, float sliderPosProportional, float rotaryStartAngle, float rotaryEndAngle, juce::Slider& slider)
+{
+    
+}
