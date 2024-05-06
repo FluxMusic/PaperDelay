d0eb6ba13677ecded8810095cd812dc94f6da453
FluxMusic <alex.huber743@gmail.com>
2024-05-06 13:25:16 +0200

    added back wrongly removed code

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -50,6 +50,9 @@ void PaperDelayAudioProcessorEditor::paint (juce::Graphics& g)
     auto msButtonBounds = timeBounds.removeFromBottom(timeBounds.getHeight() / 3);
     auto syncButtonBounds = msButtonBounds.removeFromRight(msButtonBounds.getWidth() / 2);
     
+    g.setColour(juce::Colours::red);
+    g.fillRect(timeBounds);
+    
     g.setColour (juce::Colours::darkgrey);
     g.setFont (20.0f);
     g.drawFittedText ("Paper Delay", getLocalBounds().removeFromTop(30), juce::Justification::centred, 1);
